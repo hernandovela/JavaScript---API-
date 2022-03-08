5cf1ae89bbea4aa7a2afafd617cab2f9d0c403bc
Hernando Vela <hdo.vela@gmail.com>
2022-03-08 11:42:17 -0500

    Consumir un API en JS

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch(url)
+.then( response => response.json())
+.then( data => mostrarData(data) )
+.catch( error => console.log(error))
+    const mostrarData = (data) => 
+     {
+         console.log(data)
+        let body = ""
+        for (var i = 0; i < data.length; i++)
+    {      
+        body+=`<tr><td>${data[i].id}</td><td>${data[i].title}</td><td>${data[i].body}</td></tr>`
+    }
+        document.getElementById('data').innerHTML = body
+    }
